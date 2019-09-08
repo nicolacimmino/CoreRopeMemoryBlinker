8c487f8a6369171eaad91f62c0a4418a83ba633d
Nicola Cimmino <nicola.cimmino@gmail.com>
2019-09-08 18:07:49 +0100

    Cleanup code

--- RopeBlinker.ino
+++ RopeBlinker.ino
@@ -1,27 +1,44 @@
-#include <CRC32.h>
-CRC32 crc;
 
-uint8_t rowPins[] = {3, 4, 5, 6};
-uint8_t colPins[] = {7, 8, 9, 10};
+//  Copyright (C) 2019 Nicola Cimmino
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see http://www.gnu.org/licenses/.
+//
+
+#define GREEN_LED_PIN 3
+#define RED_LED_PIN 12
+#define DRIVE_COMMON_PIN 7
+
+uint8_t drivePins[] = {2, 4, 5, 6, 8, 9, 10, 11};
 uint8_t sensePins[] = {A0, A1, A2, A3};
 
 void setup()
 {
-    for (uint8_t ix = 0; ix < 4; ix++)
-    {
-        pinMode(sensePins[ix], INPUT);
-    }
+    pinMode(RED_LED_PIN, OUTPUT);
+    pinMode(RED_LED_PIN, OUTPUT);
 
-    Serial.begin(9600);
+    Serial.begin(115200);
     analogReference(INTERNAL);
 }
 
+
 uint8_t readNibble(uint8_t address)
 {
-    uint8_t A = colPins[address & 0b11];
-    uint8_t B = rowPins[(address >> 2) & 0b11];
+    uint8_t A = drivePins[address];
+    uint8_t B = DRIVE_COMMON_PIN;
     uint8_t values[] = {0, 0, 0, 0};
 
+    // Discharge the sense capacitors.
     for (uint8_t ix = 0; ix < 4; ix++)
     {
         pinMode(sensePins[ix], OUTPUT);
@@ -33,7 +50,13 @@ uint8_t readNibble(uint8_t address)
     pinMode(A, OUTPUT);
     pinMode(B, OUTPUT);
 
-    for (uint8_t phase = 0; phase < 2; phase++)
+    // Give a couple of whacks to the drive line so we induce 
+    // current in the sense coils of the set bits. 
+    // Here we energize the drive line in both polarizations
+    // alternatively. This showed to give the best results
+    // regardless of orientation of the sense coils versus
+    // the drive lines.
+    for (uint8_t phase = 0; phase < 4; phase++)
     {
         digitalWrite(A, phase % 2);
         digitalWrite(B, (phase + 1) % 2);
@@ -42,10 +65,14 @@ uint8_t readNibble(uint8_t address)
             values[ix] += analogRead(sensePins[ix]);
         }
     }
-  
+
+    digitalWrite(A, LOW);
+    digitalWrite(B, LOW);
+
     pinMode(A, INPUT);
     pinMode(B, INPUT);
 
+    // We consider a bit set if we got anything on the sense line.
     uint8_t result = 0;
     for (uint8_t ix = 0; ix < 4; ix++)
     {
@@ -57,9 +84,9 @@ uint8_t readNibble(uint8_t address)
 
 void loop()
 {
-    crc.reset();
+    uint32_t value32 = 0;
 
-    for (int address = 0; address < 16; address++)
+    for (int address = 0; address < 8; address++)
     {
         if (address > 0 && address % 2 == 0)
         {
@@ -68,21 +95,25 @@ void loop()
 
         uint8_t nibble = readNibble(address);
 
-        crc.update(nibble);
+        value32 = (value32 << 4) | nibble;
 
         Serial.print(nibble, HEX);
+
+        // Keep breathing! See Sean Voisen great post from which I grabbed the formula.
+        // https://sean.voisen.org/blog/2011/10/breathing-led-with-arduino/
+        float val = (exp(sin(millis() / 2000.0 * PI)) - 0.36787944) * 108.0;
+        analogWrite(GREEN_LED_PIN, val);
     }
 
-    uint32_t crcValue = crc.finalize();
+    Serial.println("");
 
-    Serial.print(" - ");
-    Serial.println(crcValue, HEX);
-
-    if (crcValue != 3702334500)
+    if (value32 != 0xF05A4E43)
     {
-        Serial.println("CRC Error!");
+        digitalWrite(GREEN_LED_PIN, LOW);
+        digitalWrite(RED_LED_PIN, HIGH);
+        Serial.println("Memory read error! I'm dead.");
         while (true)
         {
-        };
+        }
     }
 }
